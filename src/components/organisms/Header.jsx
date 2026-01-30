f96a77834bdc8d11435f324f447d91f88f6054ca
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2026-01-30 11:37:11 +0530

    feat:fiux logo section

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -9,7 +9,7 @@ import CircleAvatar from "../common/CircleAvatar";
 export default function Header({ onMenuClick, user }) {
   const { name, avatar } = user || {};
   return (
-    <header className="sticky top-0 z-50 w-full bg-white shadow h-[64px] flex items-center justify-between px-4 md:px-6">
+    <header className="sticky top-0 z-50 w-full bg-white shadow h-[72px] flex items-center justify-between px-4 md:px-6">
       {/* Left Section — Logo + Menu Button */}
       <div className="flex items-center justify-between gap-3">
         {/* Mobile menu button */}
@@ -19,10 +19,10 @@ export default function Header({ onMenuClick, user }) {
 
         {/* Logo */}
         <Image
-          src="/img/npimg.png"
-          height={40}
-          width={110}
-          className="object-contain"
+          src="/img/logo.png"
+          height={120}
+          width={170}
+          className="object-cover"
           alt="Company Logo"
         />
       </div>
