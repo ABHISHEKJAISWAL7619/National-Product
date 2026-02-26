b3ac6bc678cbcaf493d884e05460e98a4bbcdfaf
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2026-02-26 12:47:35 +0530

    feat:fix  minimum percentage issue

--- src/validations/compositionSchema.js
+++ src/validations/compositionSchema.js
@@ -5,17 +5,17 @@ export const compositionSchema = Yup.object().shape({
   category: Yup.string().required("Category is required"),
   subcategory: Yup.string().required("Subcategory is required"),
   symbol: Yup.string().required("symbol is required"),
-    productCode: Yup.string().required("productCode is required"),
+  productCode: Yup.string().required("productCode is required"),
   compositions: Yup.array()
     .of(
       Yup.object().shape({
         itemId: Yup.string().required("Please select an item"),
         percentage: Yup.number()
           .typeError("Percentage must be a number")
-          .min(1, "Minimum 1% required")
+          .min(0.01, "Minimum 0.01% required")
           .max(100, "Maximum 100% allowed")
           .required("Item percentage is required"),
-      })
+      }),
     )
     .min(1, "At least one item required"),
 });
