481addd264a821edabfd06e07caaa01bc8397be8
ABHISHEKJAISWAL7619 <jaiswalabhishek7619@gmail.com>
2026-03-18 13:06:06 +0530

    feat:fix paginatiuon isue in waste management

--- src/app/waste-management/page.jsx
+++ src/app/waste-management/page.jsx
@@ -2,14 +2,17 @@ import WasteManagement from "@/components/pages/waste-management";
 import MainLayout from "@/components/templates/templates/MainLayout";
 import React, { Suspense } from "react";
 
-const page = () => {
+const page =async ({searchParams}) => {
+    let { page } = await searchParams;
+
   return (
     <MainLayout>
       <Suspense fallback={<div>Loading...</div>}>
-        <WasteManagement />
+        <WasteManagement currPage={page} />
       </Suspense>
     </MainLayout>
   );
 };
 
 export default page;
+
